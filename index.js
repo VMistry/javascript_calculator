6cad59b8067547bc76091e02adb134ebf8a80b86
VMistry <virenmistry@sky.com>
2018-08-07 22:23:28 +0100

    Half way done

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 
 choice();
 function choice(){
-  var answer = prompt("What would you like to use(type the acronym): Basic calculator(B), Basic Long calculator(BL), Advanced calculator(A), BMI calculator(BMI) or Trip calculator(T)");
+  var answer = prompt("What would you like to use(type the acronym): Basic calculator(B), Basic Long calculator(BL), Advanced calculator(A), BMI calculator(BMI) or Trip calculator(T)").toLowerCase();
+
   if(answer == "b" || answer == "basic calculator"){
     basic();
   }
-  else if(answer == "bl" || answer == "Basic Long calculator"){
+  else if(answer == "bl" || answer == "basic Long calculator"){
     basicLong();
   }
   else if(answer == "a" || answer == "advanced calculator"){
@@ -16,6 +17,9 @@ function choice(){
   }
   else if(answer == "t" || answer == "trip calculator"){
     Trip_calculator();
+  }
+  else{
+    alert("error. Reload page");
   }
 }
 
@@ -53,21 +57,21 @@ function basicLong(){
   var number2;
   var pressedEqual = false;
   while(!pressedEqual){
-    operator = prompt("Enter operation");
+    operator = prompt("Enter an operation");
     if(operator == "+"){
-      number2 = parseInt(prompt("Enter number"));
+      number2 = parseInt(prompt("Enter a number"));
       number1 += number2;
     }
     else if(operator == "-"){
-      number2 = parseInt(prompt("Enter number"));
+      number2 = parseInt(prompt("Enter a number"));
       number1 -= number2;
     }
     else if(operator == "*"){
-      number2 = parseInt(prompt("Enter number"));
+      number2 = parseInt(prompt("Enter a number"));
       number1 *= number2;
     }
     else if(operator == "/"){
-      number2 = parseInt(prompt("Enter number"));
+      number2 = parseInt(prompt("Enter a number"));
       number1 /= number2;
     }
     else if(operator == "="){
@@ -80,18 +84,42 @@ function basicLong(){
 
 
 function Advanced(){
- var number1 = parseInt(prompt("Enter number"));
- var power = parseInt(prompt("Enter power"));
- number1 = number1**power;
- alert(number1);
+  var answer = prompt("Which function would you like to us? Square Root(sr) or Power(p).").toLowerCase();
+  if(answer == "sr" || answer == "square root"){
+    var number1 = parseInt(prompt("Enter a number"));
+    number1 = Math.sqrt(number1);
+    number1 = Math.round( number1 * 10 ) / 10;
+    alert(number1);
+  }
+
+  else if(answer == "p" || answer == "power") {
+    var number1 = parseInt(prompt("Enter a number"));
+    var power = parseInt(prompt("Enter a power"));
+    number1 = Math.pow(number1, power);
+    number1 = Math.round( number1 * 10 ) / 10;
+    alert(number1);
+  }
+  else{
+    alert("error. Reload page");
+  }
 }
+
 function BMI(){
-
-
+  var weight = parseFloat(prompt("Type in your weight in kg"));
+  var height = parseFloat(prompt("Type in your height in meters"));
+  var result = (weight / height) / height;
+  result = Math.round( result * 10 ) / 10;
+  alert(result);
 }
 
 function Trip_calculator(){
+  var distance = parseFloat(prompt("Type in How far you travelled in kilometers"));
+  var fuel_consumption = parseFloat(prompt("Type in your fuel consumption"));
+  var cost = parseFloat(prompt("Type in how much per litres fuel costs"));
 
-
-
+  var result = distance/100;
+  result = result * fuel_consumption;
+  result = result * cost;
+  result = Math.round( result * 100 ) / 100;
+  alert(result);
 }
